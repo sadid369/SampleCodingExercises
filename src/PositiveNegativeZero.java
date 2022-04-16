a7ae28c88082e083db4a7842af3cbe89fc520e7d
Sadid369 <sadid.jones@gmail.com>
2022-04-16 23:39:47 +0600

    Initial commit

---
+++ src/PositiveNegativeZero.java
@@ -0,0 +1,16 @@
+public class PositiveNegativeZero {
+    public static void main(String[] args) {
+    checkNumber(0);
+    }
+    public static void checkNumber(int number){
+      if(number>0) {
+          System.out.println("postive");
+      } else if(number<0){
+          System.out.println("nagative");
+      }
+      else{
+          System.out.println("zero");
+      }
+      }
+    }
+
